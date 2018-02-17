30c17f3f28c5ffca5837dc9893f58de828f3cf7d
fmccready <fmccready@gmail.com>
2018-02-17 12:11:03 -0600

    Finish hello world

---
+++ js/ClientApp.jsx
@@ -0,0 +1,23 @@
+const ce = React.createElement
+
+const MyTitle = function(props){
+  return (
+    ce('div', undefined,
+      ce('h1', {style: {color: props.color}}, props.title)
+    )
+  )
+}
+const MyFirstComponent = function(){
+  return ce('div', {id: 'my-first-component'},
+    [
+      ce(MyTitle, {title: 'Game of Thrones', color: 'YellowGreen'}),
+      ce(MyTitle, {title: 'Stranger Things', color: 'GreenYellow'}),
+      ce(MyTitle, {title: 'Rick and Morty', color: 'cornflowerBlue'}),
+      ce(MyTitle, {title: 'Silicon Valley', color: 'olive'})
+    ]
+  )
+}
+ReactDOM.render(
+  ce(MyFirstComponent),
+  document.getElementById('app')
+)
